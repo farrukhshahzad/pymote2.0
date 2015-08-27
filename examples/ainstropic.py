163d1bad29b5026621e26235435bbf59d2cb9f21
farrukh.shahzad <farrukhshahzad2002@hotmail.com>
2015-08-27 13:23:30 +0300

    Cleanup, added new topologies

--- examples/ainstropic.py
+++ examples/ainstropic.py
@@ -4,14 +4,14 @@ import scipy as sp
 
 from pymote import *
 from pymote.conf import global_settings
-from beacon import Beacon, MAX_TRIES
+
 from pymote import propagation
-from toplogies import Star
+from toplogies import Toplogy
 from pymote.utils import plotter
 from pymote.utils.filing import get_path, date2str,\
      DATA_DIR, TOPOLOGY_DIR, CHART_DIR, DATETIME_DIR
 import numpy as np
-
+from numpy.random import seed
 from numpy import array, sqrt, power
 
 from pymote.algorithms.niculescu2003.trilaterate import Trilaterate
@@ -20,6 +20,8 @@ from pymote.sensor import TruePosSensor
 from pymote.networkgenerator import NetworkGenerator
 from pymote.algorithms.niculescu2003.dvhop import DVHop
 
+
+seed(123)  # to get same random sequence for each run
 
 # Network/Environment setup
 global_settings.ENVIRONMENT2D_SHAPE = (600, 600)
@@ -37,18 +39,18 @@ y_radius = h/3/clusters
 
 degree = 10
 Node.cid = 1
-net_gen = NetworkGenerator(n_count=n, degree=degree, n_max=n, n_min=n, connected=False)
-net = net_gen.generate_random_network()
-n_anchors = (int)(p_anchors * net.__len__()/100.0)
-for node in net.nodes()[:20]:
+net_gen = Toplogy(n_count=n, degree=degree, n_max=n, n_min=n, connected=False)
+net = net_gen.generate_grid_network(randomness=0.2)
+n_anchors = (int)(p_anchors * 100 /net.__len__())
+for node in net.nodes():
+    if (node.id % n_anchors==0):  # anchor nodes
         node.compositeSensor = (TruePosSensor,)
         node.type = 'C'  # Anchors
 
-net.name = "random Deg: %s" %degree
 # saving topology as PNG image
 avg_deg = round(net.avg_degree())
-net.savefig(fname=get_path(TOPOLOGY_DIR, "Random-%s-%i" %(n, avg_deg)),
-            title="%s - %s nodes, %s Avg. degree" % (net.name, n, avg_deg),
+net.name = "Grid - Nodes=%s, Avg degree=%s, Range=%s" % (net.__len__(), int(avg_deg), int(node.commRange))
+net.savefig(fname=get_path(TOPOLOGY_DIR, net.name),   title=net.name,
             x_label="X", y_label="Y", show_labels=False)
 
 print net.__len__(), avg_deg, node.commRange
@@ -59,14 +61,29 @@ net.reset()
 Node.cid = 1
 net_gen = NetworkGenerator(n_count=n, degree=degree)
 net = net_gen.generate_homogeneous_network()
-n_anchors = (int)(p_anchors * net.__len__()/100.0)
+n_anchors = (int)(p_anchors * 100 /net.__len__())
 for node in net.nodes():
-    if (node.id % p_anchors==0):  # anchor nodes
+    if (node.id % n_anchors==0):  # anchor nodes
         node.compositeSensor = (TruePosSensor,)
         node.type = 'C'  # Anchors
+
 avg_deg = round(net.avg_degree())
-net.savefig(fname=get_path(TOPOLOGY_DIR, "Homogenous-%s-%i" %(n, avg_deg)),
-            title="Homogeneous - %s nodes, %s Avg degree" % (n, avg_deg),
+net.name = "Homogenous - Nodes=%s, Avg degree=%s, Range=%s" % (net.__len__(), int(avg_deg), int(node.commRange))
+net.savefig(fname=get_path(TOPOLOGY_DIR, net.name),
+            title=net.name,
             x_label="X", y_label="Y", show_labels=False)
 print net.__len__(),avg_deg,  node.commRange, n_anchors
+net.reset()
 
+# network topology setup
+settings.COMM_RANGE = 50
+settings.ENVIRONMENT2D_SHAPE = (200, 200)
+Node.cid = 1
+net_gen = Toplogy()
+net = net_gen.generate_manual_network()
+node = net.nodes()[1]
+avg_deg = round(net.avg_degree())
+net.name = "Manual - Nodes=%s, Avg degree=%s, Range=%s" % (net.__len__(), int(avg_deg), int(node.commRange))
+net.savefig(fname=get_path(TOPOLOGY_DIR, net.name),
+            title=net.name, x_label="X", y_label="Y", show_labels=True)
+print net.__len__(),avg_deg,  node.commRange
